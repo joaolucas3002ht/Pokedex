c36ff4fdc2fe51b54e7aeab0e0f87942f14bb28f
joaolucas3002 <92446800+joaolucas3002@users.noreply.github.com>
2023-01-07 02:23:58 -0300

    edit component type

--- src/components/Type/index.tsx
+++ src/components/Type/index.tsx
@@ -8,11 +8,11 @@ export const Type = ({ type, damageValue }: TypeProps) => {
 
    return (
       <div
-         className={` py-1 px-2 rounded-2xl font-bold text-zinc-800 text-[0.625rem] leading-[0.875rem] capitalize ${gb} md:text-sm md:py-1 md:px-3 flex gap-1 justify-center items-center`}
+         className={`h-[1.5rem] py-1 px-3 rounded-2xl  font-bold text-zinc-800 text-[0.625rem] leading-[0.875rem] capitalize ${gb} md:text-sm md:h-[2rem] md:py-1 md:px-3 md:rounded-3xl flex gap-1 justify-center items-center`}
       >
          <span>{type} </span>
          {damageValue && (
-            <span className="bg-zinc-200/40 p-[.125rem] rounded  md:p-1">
+            <span className="bg-zinc-200/40 p-[.125rem] rounded  ">
                {damageValue}
             </span>
          )}
